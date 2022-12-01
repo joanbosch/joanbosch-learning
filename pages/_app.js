8d2691eaa868f0e28dd37a07c628ec2ff90d7e95
Joan Bosch <jbosch@adiquimica.com>
2022-12-01 20:00:49 +0100

    Add Data Flow

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
+import { Analytics } from '@vercel/analytics/react';
 import 'nextra-theme-docs/style.css'
 
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return 
+  <>
+    <Component {...pageProps} />
+    <Analytics />
+  </>
 }
